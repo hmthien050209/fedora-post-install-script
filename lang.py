2d83adbcbfbe265318f5e74f974c25000f729177
Hoàng Minh Thiên <hoangminhthien05022009@gmail.com>
2021-11-10 19:06:19 +0700

    Added Multi-language support

---
+++ lang.py
@@ -0,0 +1,77 @@
+CascadiaFontsInstallMSG = {
+    "en": "Installing Cascadia Code Fonts and Powerline to make yout Terminal better...",
+    "vi": "Đang cài PowerLine và font Cascadia Code giúp Terminal đẹp hơn..."
+}
+getDraculaThemeMSG = {
+    "en": "Installing Dracula theme...",
+    "vi": "Đang tải Dracula theme giúp cho giao diện trông đẹp hơn..."
+}
+UpdateMSG = {
+    "en": "Updating your system...",
+    "vi": "Đang cập nhật hệ điều hành của bạn..."
+}
+DNFWriteMSG = {
+    "en": "Optimizing DNF package manager config...",
+    "vi": "Đang tối ưu hóa trình quản lý gói phần mềm DNF..."
+}
+EnableRPMFusionMSG = {
+    "en": "Enabling RPM Fusion...",
+    "vi": "Đang bật RPM Fusion để tải các phần mềm mã nguồn đóng/trả phí..."
+}
+EnableFlathubMSG = {
+    "en": "Enabling Flathub...",
+    "vi": "Đang bật Flathub để tải các phần mềm mã nguồn đóng/trả phí..."
+}
+EnableSnapdMSG = {
+    "en": "Enabling snapd",
+    "vi": "Đang bật snapd để tải các phần mềm mã nguồn đóng/trả phí..."
+}
+CodecsInstallMSG = {
+    "en": "Installing Multimedia codecs...",
+    "vi": "Đang tải một số codec/thư viện cần thiết cho Multimedia..."
+}
+ToolsInstallMSG = {
+    "en": "Installing some tools...",
+    "vi": "Đang cài một số tool cần thiết..."
+}
+
+CoolBootModeMSG = {  # Just for fun
+    "en": "Doing some magics that can help you become a cool guy :)",
+    "vi": "Đang làm một bí thuật giúp bạn ngầu hơn khi xài Linux :)"
+}
+CleanUpMSG = {
+    "en": "Cleaning up...",
+    "vi": "Đang dọn một số tập tin tạm thời..."
+}
+BackUpMSG = {
+    "en": "Backing up...",
+    "vi": "Đang backup lại một số file phòng trường hợp máy bị lỗi..."
+}
+greeting = {
+    "en": "Welcome to Fedora Post-install script written by davidhoang05022009(Hoàng Minh Thiên)",
+    "vi": "Chào mừng bạn đến với script Post-install dành cho Fedora Linux viết bởi davidhoang05022009(Hoàng Minh Thiên)\n"
+}
+sudoReminder = {
+    "en": "If you're not sure that did you run this script as root, rerun it by typing \'sudo python3 post-install\' in the Terminal",
+    "vi": "Bạn cần chạy lệnh này dưới quyền root. Nếu không biết bạn đã ở quyền root hay chưa, hãy hủy và khởi động lại chương trình bằng lệnh \'sudo python3 post-install\'"
+}
+acceptedMSG = {
+    "en": "OK, let the script do its works, it won't take long...",
+    "vi": "OK, hãy để script thực hiện công việc của mình, sẽ không lâu đâu..."
+}
+canceledMSG = {
+    "en": "Aborted",
+    "vi": "Đã hủy"
+}
+doneMSG = {
+    "en": "Done, do you want to restart now?",
+    "vi": "Xong, bạn có muốn khởi động lại máy không?"
+}
+doneWithoutRestartMSG = {
+    "en": "OK, remember to restart your device",
+    "vi": "Nhớ khởi động lại máy của bạn nhé ^^"
+}
+confirmation = {
+    "en": "Continue?",
+    "vi": "Bạn sẽ tiếp tục để script này chạy chứ?"
+}
--- post-install.py
+++ post-install.py
@@ -1,14 +1,28 @@
 import datetime
 import os
 from zipfile import ZipFile
+from lang import *
+import sys
 
 
 def log(stage):
     print('[' + str(datetime.datetime.now()) + ']: ' + stage)
 
 
+def getUserLanguage():
+    lang = input("[1] Tiếng Việt/Vietnamese\n[2] Tiếng Anh/English")
+    if lang == 1:
+        return "vi"
+    elif lang == 2:
+        return "en"
+    else:
+        print("Invalid input, aborting...")
+        sys.exit()
+
+userLanguage = getUserLanguage()
+
 def installPowerline():
-    log('Đang cài PowerLine và font Cascadia Code giúp Terminal đẹp hơn...')
+    log(CascadiaFontsInstallMSG[userLanguage])
     os.system('sudo dnf install powerline -y')
     fRead = open('./.bashrc', 'r')
     fo = open(str(os.path.expanduser('~'))+'/.bashrc', 'w')
@@ -23,7 +37,7 @@ def installPowerline():
 
 
 def getDraculaTheme():
-    log('Đang tải Dracula theme giúp cho giao diện trông đẹp hơn')
+    log(getDraculaThemeMSG[userLanguage])
     os.system('axel -n 20 https://github.com/dracula/gtk/archive/master.zip')
     ZipFile('./gtk-master.zip', 'r') .extractall('/usr/share/themes/')
     os.system('gsettings set org.gnome.desktop.interface gtk-theme \'gtk-master\' && gsettings set org.gnome.desktop.wm.preferences theme \'gtk-master\'')
@@ -32,12 +46,12 @@ def getDraculaTheme():
 
 
 def doUpdateAndUpgrade():
-    log('Đang cập nhật hệ thống...')
+    log(UpdateMSG[userLanguage])
     os.system('sudo dnf update -y && sudo dnf upgrade -y')
 
 
 def writeNewDNFConfig():
-    log('Đang tối ưu hóa trình quản lý gói phần mềm DNF...')
+    log(DNFWriteMSG[userLanguage])
     fRead = open('./dnf.conf', 'r')
     fo = open('/etc/dnf/dnf.conf', 'w')
     fo.write(fRead.read())
@@ -47,38 +61,37 @@ def writeNewDNFConfig():
 
 
 def enableRPMFusion():
-    log('Đang bật RPM Fusion để tải các phần mềm mã nguồn đóng/trả phí...')
+    log(EnableRPMFusionMSG[userLanguage])
     os.system('sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y')
     os.system('sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y')
 
 
 def enableFlathub():
-    log('Đang bật Flathub để tải các phần mềm mã nguồn đóng/trả phí...')
+    log(EnableFlathubMSG[userLanguage])
     os.system(
         'flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo')
 
 
 def enableSnapd():
-    log('Đang bật snapd để tải các phần mềm mã nguồn đóng/trả phí...')
+    log(EnableSnapdMSG[userLanguage])
     os.system('sudo dnf install snapd -y')
 
 
 def installCodecs():
-    log('Đang tải một số codec/thư viện cần thiết cho Multimedia...')
+    log(CodecsInstallMSG[userLanguage])
     os.system(
         'sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y')
     os.system('sudo dnf install lame\* --exclude=lame-devel -y')
     os.system('sudo dnf group upgrade --with-optional Multimedia -y')
 
 
-def installSoftware():
-    log('Đang cài một số tool cần thiết...')
+def installTools():
+    log(ToolsInstallMSG[userLanguage])
     os.system('sudo dnf install htop neofetch xclip gedit axel git gnome-tweaks -y')
 
 
 def uninstallPlymouthAndEnableVerboseBootMode():
-    # Thật ra hàm này tắt giao diện khởi động chỉ để cho ngầu :))
-    log('Đang làm một bí thuật giúp bạn ngầu hơn khi xài Linux :))')
+    log(CoolBootModeMSG[userLanguage])
     fRead = open('./grub', 'r')
     fo = open('/etc/default/grub', 'w')
     fo.write(fRead.read())
@@ -92,16 +105,21 @@ def uninstallPlymouthAndEnableVerboseBootMode():
 
 
 def cleanUp():
-    log('Đang dọn một số tập tin tạm thời...')
+    log(CleanUpMSG[userLanguage])
     os.system('sudo rm -rf ./CascadiaCode-2110.31 ./gtk-master.zip')
 
 
 def backUp():
-    log('Đang backup lại một số file phòng trường hợp máy bị lỗi...')
+    log(BackUpMSG[userLanguage])
 
     #Backup dnf.conf
     fReadDNFConf = open('/etc/dnf/dnf.conf', 'r')
-    fWriteDNFConfBackup = open('./dnf.conf.bak', 'x')
+    fWriteDNFConfBackup = None
+    # Check if the file exists to make sure that the FileExistsError will never happen...
+    if os.path.exists('./dnf.conf.bak'):
+        fWriteDNFConfBackup = open('./dnf.conf.bak', 'w')
+    else:
+        fWriteDNFConfBackup = open('./dnf.conf.bak', 'x')
     fWriteDNFConfBackup.write(fReadDNFConf.read())
     fWriteDNFConfBackup.flush()
     fWriteDNFConfBackup.close()
@@ -109,33 +127,40 @@ def backUp():
 
     #Backup .bashrc
     fReadBASHRC = open(str(os.path.expanduser('~'))+'/.bashrc', 'r')
-    fWriteBASHRCBackup = open('./.bashrc.bak', 'x')
+    fWriteBASHRCBackup = None
+    # Check if the file exists to make sure that the FileExistsError will never happen...
+    if os.path.exists('./.bashrc'):
+        fWriteBASHRCBackup = open('./.bashrc.bak', 'w')
+    else:
+        fWriteBASHRCBackup = open('./.bashrc.bak', 'x')
     fWriteBASHRCBackup.write(fReadBASHRC.read())
     fWriteBASHRCBackup.flush()
     fWriteBASHRCBackup.close()
     fReadBASHRC.close()
 
     #Backup grub
-    fReadgrub = open('/etc/default/grub', 'r')
-    fWritegrubBackup = open('./grub.bak', 'x')
+    #Backup .bashrc
+    fReadgrub = open(str(os.path.expanduser('~'))+'/.grub', 'r')
+    fWritegrubBackup = None
+    # Check if the file exists to make sure that the FileExistsError will never happen...
+    if os.path.exists('./.grub'):
+        fWritegrubBackup = open('./grub.bak', 'w')
+    else:
+        fWritegrubBackup = open('./grub.bak', 'x')
     fWritegrubBackup.write(fReadgrub.read())
     fWritegrubBackup.flush()
     fWritegrubBackup.close()
     fReadgrub.close()
-    log("Đã backup xong...")
 
-
-print('Chào mừng bạn đến với script init Fedora Linux viết bởi davidhoang05022009(Hoàng Minh Thiên)\n')
-print('Bạn cần chạy lệnh này dưới quyền root. Nếu không biết bạn đã ở quyền root hay chưa, hãy hủy và khởi động lại chương trình bằng lệnh \'sudo python3 -m post-install\'')
-print('Nếu đây là lần đầu bạn tiếp xúc với Linux thì script này sẽ giúp bạn cài đặt những thứ cần thiết để sử dụng trong việc học tập và làm việc hàng ngày\n')
-confirm = input('Bạn sẽ tiếp tục để script này chạy chứ? [y(es)/n(o)]: ')
+print(greeting[userLanguage])
+print(sudoReminder[userLanguage])
+confirm = input(confirmation[userLanguage] + ' [y(es)/n(o)]: ')
 if confirm.lower() == 'y' or confirm.lower() == 'yes':
-    print('Bạn hãy đợi 10 phút để phần mềm làm việc cho bạn, phần mềm cần mật khẩu để chình sửa một vài thông tin hệ thống, nên bạn yên tâm nhập vào nha ^^')
-    print('\nHãy đi pha 1 tách cà phê và chờ/đi tập thể dục...')
+    print(acceptedMSG[userLanguage])
     backUp()
     doUpdateAndUpgrade()
     writeNewDNFConfig()
-    installSoftware()
+    installTools()
     enableRPMFusion()
     enableFlathub()
     enableSnapd()
@@ -144,12 +169,11 @@ if confirm.lower() == 'y' or confirm.lower() == 'yes':
     getDraculaTheme()
     installPowerline()
     cleanUp()
-    print('Đã hoàn thành...')
     confirm2 = input(
-        'Bạn có muốn khởi động lại máy ngay bây giờ không? [y(es)/n(o)]: ')
+        doneMSG[userLanguage] + ' [y(es)/n(o)]: ')
     if confirm2.lower() == 'y' or confirm2.lower() == 'yes':
         os.system('sudo systemctl reboot')
     else:
-        print('Nhiệm vụ của script đã hoàn tất, chỉ cần khới động lại là xong việc...')
+        print(doneWithoutRestartMSG[userLanguage])
 else:
-    print('Đã hủy lệnh...')
+    print(canceledMSG[userLanguage])
