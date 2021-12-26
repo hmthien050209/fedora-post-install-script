69d16c27f09ebea2fee93dfe9b21d708a21a6ba1
Hoàng Minh Thiên <hoangminhthien05022009@gmail.com>
2021-12-26 10:15:31 +0700

    Update post-install.py

--- post-install.py
+++ post-install.py
@@ -26,7 +26,7 @@ userLanguage = getUserLanguage()
 
 def installPowerline():
     log(CascadiaFontsInstallMSG[userLanguage])
-    
+
     os.system('sudo dnf install powerline -y')
     fRead = open('./.bashrc', 'r')
     os.system('echo ' + fRead.read() + '>> ~/.bashrc')
@@ -37,7 +37,8 @@ def installPowerline():
         'sudo mv ./CascadiaCode-2110.31/ttf/static/* /usr/share/fonts; fc-cache -f -v')
     os.system('source ~/.bashrc')
     # Set the terminal font
-    os.system('dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal-profiles.dconf')
+    os.system(
+        'dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal-profiles.dconf')
 
 
 def getDraculaTheme():
@@ -49,13 +50,19 @@ def getDraculaTheme():
     os.system(
         'gsettings set org.gnome.desktop.wm.preferences button-layout \":minimize,maximize,close\"')
 
+
 def installFonts():
     log(fontsInstallMSG[userLanguage])
     os.system('sudo dnf install google-*-fonts -x *cjk* --skip-broken -y')
-    os.system('gsettings set org.gnome.desktop.interface font-name \'Noto Sans Medium 11\'')
-    os.system('gsettings set org.gnome.desktop.interface document-font-name \'Noto Sans Regular 11\'')
-    os.system('gsettings set org.gnome.desktop.interface monospace-font-name \'Cascadia Code PL 13\'')
-    os.system('gsettings set org.gnome.desktop.wm.preferences titlebar-font \'Noto Sans Bold 11\'')
+    os.system(
+        'gsettings set org.gnome.desktop.interface font-name \'Noto Sans Medium 11\'')
+    os.system(
+        'gsettings set org.gnome.desktop.interface document-font-name \'Noto Sans Regular 11\'')
+    os.system(
+        'gsettings set org.gnome.desktop.interface monospace-font-name \'Cascadia Code PL 13\'')
+    os.system(
+        'gsettings set org.gnome.desktop.wm.preferences titlebar-font \'Noto Sans Bold 11\'')
+
 
 def doUpdate():
     log(UpdateMSG[userLanguage])
@@ -78,6 +85,7 @@ def enableFlathub():
     os.system(
         'flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo')
 
+
 def installCodecs():
     log(CodecsInstallMSG[userLanguage])
     os.system(
@@ -91,21 +99,27 @@ def installTools():
     os.system(
         'sudo dnf install htop neofetch xclip gedit axel git gnome-tweaks deltarpm -y')
 
-def installIbusBamboo(): # only needed for Vietnamese
+
+def installIbusBamboo():  # only needed for Vietnamese
     log("Đang cài bộ gõ tiếng Việt ibus-bamboo để hỗ trợ các bạn gõ tiếng Việt")
 
     # check the version of Fedora
     fedora_version = 0
     os.system('rpm -E %fedora > fedora-version')
     fRead = open('./fedora-version', 'r')
-    if int(fRead.read()) > 33: fedora_version = 33
-    else: fedora_version = int(fRead.read())
-    
+    if int(fRead.read()) > 33:
+        fedora_version = 33
+    else:
+        fedora_version = int(fRead.read())
+
     #install it and add it to input sources
-    os.system('sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:lamlng/Fedora_'+ fedora_version +'/home:lamlng.repo')
+    os.system('sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:lamlng/Fedora_' +
+              fedora_version + '/home:lamlng.repo')
     os.system('sudo dnf install ibus-bamboo -y')
-    os.system('gsettings set org.gnome.desktop.input-sources sources [(\'xkb\', \'us\'), (\'ibus\', \'Bamboo::Us\')]')
+    os.system(
+        'gsettings set org.gnome.desktop.input-sources sources [(\'xkb\', \'us\'), (\'ibus\', \'Bamboo::Us\')]')
     os.system('gsettings set org.gnome.desktop.interface gtk-im-module \'ibus\'')
+
 
 def uninstallPlymouthAndEnableVerboseBootMode():
     log(CoolBootModeMSG[userLanguage])
@@ -129,6 +143,7 @@ def backUp():
     os.system("sudo cp ~/.bashrc ~/.bashrc.bak")
     os.system("sudo cp /etc/default/grub /etc/default/grub.bak")
 
+
 print(greeting[userLanguage])
 print(sudoReminder[userLanguage])
 confirm = input(confirmation[userLanguage] + ' [y(es)/n(o)]: ')
@@ -146,7 +161,8 @@ if confirm.lower() == 'y' or confirm.lower() == 'yes':
     getDraculaTheme()
     installPowerline()
     installFonts()
-    if userLanguage == 'vi': installIbusBamboo()
+    if userLanguage == 'vi':
+        installIbusBamboo()
     cleanUp()
     confirm2 = input(
         doneMSG[userLanguage] + ' [y(es)/n(o)]: ')
